e62b75ff76fd361a6cd8ac896a73e4dae9de0069
yvanzewo <yvan.dongmo.zewo@ulb.be>
2025-11-17 11:15:17 +0100

    first lab all files

---
+++ CRP_white_lab1_EE/IdentificationCode_CRP.m
@@ -0,0 +1,11 @@
+u=DataCommands.';
+y=Data(:,1).';
+%Operating point (u0,y0) = (0.9, Data(1,1)) V
+u_offset=u0.'; % input Operating point
+y_offset = Data(1,1).'; % output operating point
+SystemOrder=[0 1]; %Number of zeros and of poles (0 and 1), respectively.
+sysIdent=IdentifySystem_CRP(u-u_offset,y-y_offset,SystemOrder,Ts)
+figure
+plot(time,y-y_offset,'.');
+hold on;
+lsim(sysIdent,u-u_offset,time);
