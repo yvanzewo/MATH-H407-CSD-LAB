f969ad082837502e684c83ebf27ab9f2f2bb83e4
yvanzewo <yvan.dongmo.zewo@ulb.be>
2025-11-18 10:52:13 +0100

    Add validation simulation of motor TF and motor control simulation with P

---
+++ CRP_lab2/Margin_control_motor_simulation.m
@@ -0,0 +1,22 @@
+close all; clear; clc;
+% Acess the stability margin of the control motor simulation;
+
+load('data for tf of motor 2.mat'); %load exp data
+k_p = 1.938; % proportional gain for P controller
+D_motor = k_p; 
+TF_motor = tf([3.401], [0.9531 1]); % Transfer function of Motor around the choosen operating point
+Delay = Ts; %  Delay define the calculation time of ADC and DAC; also the ZOH impact
+%Anti alaising filter: 2nd order Butterworth filter
+omega_n = 2*pi*fc ;
+zeta = 0.7;
+Filter_AA = tf([omega_n^2],[1 2*omega_n*zeta omega_n^2]); % TF of filter
+OP_motor_1 = D_motor*TF_motor; % open loop of motor without delay and filter;
+OP_motor_1.InputDelay = Delay; % Add delay on the inner loop of motor
+OP_motor_2 = OP_motor_1*Filter_AA; % open loop of motor with filter and delay
+
+[Gm1,Pm1] = margin(OP_motor_1); % gain and phase margin without filter
+[Gm2,Pm2] = margin (OP_motor_2); % gain and phase margin with filter and delay
+figure;
+margin(OP_motor_1);
+figure;
+margin (OP_motor_2);
--- CRP_lab2/good_identify_system.m
+++ CRP_lab2/good_identify_system.m
@@ -7,11 +7,11 @@ finish = 30/Ts;%Final time to record the Transfer function
 u_offset=u(1,start); % input Operating point (normally is u_offset = u0 = 0.48 V choosen point to have 5 rad/s)
 y_offset = y(1,start); %y0 output operating point
 SystemOrder=[0 1]; %Number of zeros and of poles (0 and 1), respectively.
-sysIdent=IdentifySystem_CRP(u(start:finish)-u_offset,y(start:finish)-y_offset,SystemOrder,Ts)
+sysIdent2=IdentifySystem_CRP(u(start:finish)-u_offset,y(start:finish)-y_offset,SystemOrder,Ts)
 figure
 plot(time(start:finish),y(start:finish)-y_offset,'.');
 hold on;
-lsim(sysIdent,u(start:finish)-u_offset,time(start:finish));
+lsim(sysIdent2,u(start:finish)-u_offset,time(start:finish));
 
 %Note that our experiment in lab said for an input current of 0.48V, will
 %have a velocity of around 2.42V :the value obtained after convertion that states 1V
